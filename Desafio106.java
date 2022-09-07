62415fe6735d73c206d53eb648c9e22dada1a484
Jorge Arce <jorgearceruiz97@gmail.com>
2022-09-07 12:07:07 +0200

    desafio 106 funcional.

--- Desafio106.java
+++ Desafio106.java
@@ -37,78 +37,88 @@ public class Desafio106 {
         String pais = "";
         int digitosPaises, digitosPaises2;
 
-        casos = sc.nextInt();
-        sc.nextLine();
-        for (int i = 0; i < casos; i++) {
-            suma = 0;
-            cod = sc.nextLine();
-            //guardo el ultimo dígito que compararé después
-            ultimoDigito = Integer.parseInt(String.valueOf(cod.charAt(cod.length()-1)));
+        casos = 1;
 
-            //creo nuevo String, quitándo el último dígito y añadiendo el 0
-            if(cod.length()<8){
-                while(cod.length()<8){
-                    //le añado 0
-                    cod += '0';
-                }
-            }else if(cod.length()>8 && cod.length()<13){
-                while(cod.length()<13){
-                    cod += '0';
+        while(casos!=0){
 
-                }
-                //para comprobar de que país es:
-                digitosPaises = Integer.parseInt(cod.substring(0,1));
-                digitosPaises2 = Integer.parseInt(cod.substring(0,2));
-                if (digitosPaises == 0) {
-                    pais = "EEUU";
-                }else if(digitosPaises2 == 50){
-                    pais = "Inglaterra";
-                }else{
-                    digitosPaises = Integer.parseInt(cod.substring(0,3));
-                    switch (digitosPaises){
-                        case 380:
-                            pais = "Bulgaria";
-                            break;
-                        case 539:
-                            pais = "Irlanda";
-                            break;
-                        case 560:
-                            pais = "Portugal";
-                            break;
-                        case 70:
-                            pais = "Noruega";
-                            break;
-                        case 759:
-                            pais = "Venezuela";
-                            break;
-                        case 850:
-                            pais = "Cuba";
-                            break;
-                        case 890:
-                            pais = "India";
-                            break;
+            casos = sc.nextInt();
+            sc.nextLine();
+
+            for (int i = 0; i < casos; i++) {
+                suma = 0;
+                cod = sc.nextLine();
+                //guardo el ultimo dígito que compararé después
+                ultimoDigito = Integer.parseInt(String.valueOf(cod.charAt(cod.length()-1)));
+
+                //creo nuevo String, quitándo el último dígito y añadiendo el 0
+                if(cod.length()<8){
+                    while(cod.length()<8){
+                        //le añado 0
+                        cod += '0';
+                    }
+                }else if(cod.length()>8 && cod.length()<=13){
+                    while(cod.length()<13){
+                        cod += '0';
+
+                    }
+                    //para comprobar de que país es:
+
+                    digitosPaises = Integer.parseInt(cod.substring(0,1));
+                    digitosPaises2 = Integer.parseInt(cod.substring(0,2));
+                    if (digitosPaises == 0) {
+                        pais = "EEUU";
+                    }else if(digitosPaises2 == 50){
+                        pais = "Inglaterra";
+                    }else{
+                        digitosPaises = Integer.parseInt(cod.substring(0,3));
+                        switch (digitosPaises){
+                            case 380:
+                                pais = "Bulgaria";
+                                break;
+                            case 539:
+                                pais = "Irlanda";
+                                break;
+                            case 560:
+                                pais = "Portugal";
+                                break;
+                            case 70:
+                                pais = "Noruega";
+                                break;
+                            case 759:
+                                pais = "Venezuela";
+                                break;
+                            case 850:
+                                pais = "Cuba";
+                                break;
+                            case 890:
+                                pais = "India";
+                                break;
+                            default:
+                                pais = "Desconocido";
+                                break;
+                        }
                     }
                 }
-            }
 
-            //se comprueba que el último dígito es correcto
-            //empezando por la derecha
-            int cont = 1; //este contador va a usarse para saber si el número al que accedemos es par o impar
-            for (int j = cod.length()-2; j > 0; j--) {//cod.length()-2 porque empiezo desde dos posiciones antes
-                if(cont%2!=0 || cont==1){
-                    suma += Integer.parseInt(String.valueOf(cod.charAt(j)))*3;
-                }else{
-                    suma += Integer.parseInt(String.valueOf(cod.charAt(j)));
+                //se comprueba que el último dígito es correcto
+                //empezando por la derecha
+                int cont = 1; //este contador va a usarse para saber si el número al que accedemos es par o impar
+                for (int j = cod.length()-2; j >= 0; j--) {//cod.length()-2 porque empiezo desde dos posiciones antes
+                    if(cont%2!=0 || cont==1){
+                        suma += Integer.parseInt(String.valueOf(cod.charAt(j)))*3;
+                    }else{
+                        suma += Integer.parseInt(String.valueOf(cod.charAt(j)));
+                    }
+                    ++cont;
                 }
-                ++cont;
-            }
 
-            if((suma+ultimoDigito)%10==0){
-                System.out.println("SI " + pais);
-            }else{
-                System.out.println("NO " + pais);
-            }
+                if((suma+ultimoDigito)%10==0){
+                    System.out.println("SI " + pais);
+                }else{
+                    System.out.println("NO " + pais);
+                }
 
+            }
 
         }
 
