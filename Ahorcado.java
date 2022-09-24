367fe753f109e310a37bc478b9147fcc0819e665
Jorge Arce <jorgearceruiz97@gmail.com>
2022-09-24 19:20:32 +0200

    Estoy haciendo un ahorcado sin terminar ni probar.
    I'm doing an hanging game, it's not finished and not prove it already.

--- Ahorcado.java
+++ Ahorcado.java
@@ -3,19 +3,11 @@ package desafios;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/*
-este ahorcado guarda unas palabras en un array, tu cometido es adivinar la palabra que te piden en el momento
-las letras estarán ocultas por ____ que será igual a la longuitud de la palabra
-cada vez que adivines una letra se sustituirá un _ por la letra correspondiente
-Si no adivinas una letra se añadiendo un dibujo del ahorcado y restando uno al contador máximo(10)
 
-1. pide la palabra
-2. comprueba si la palabra es igual que la palabra a adivinar
- */
 public class Ahorcado {
     Scanner sc;
     ArrayList<String>palabrasAdivinar;
-    int cont = 10;
+    int cont = 0;
 
     public Ahorcado() {
         palabrasAdivinar = new ArrayList<>();
@@ -30,7 +22,7 @@ public class Ahorcado {
     }
 
     private int chooseNumberXSelectWord(){
-        int num = (int) Math.random();
+        int num = (int) Math.random()*4;
         return num;
     }
 
@@ -44,7 +36,7 @@ public class Ahorcado {
     }
 
 
-    private String getHangedWord(int num, String word){
+    private String getHangedWord(int num){
         String hangedWord = palabrasAdivinar.get(num);
         return hangedWord;
     }
@@ -56,9 +48,10 @@ public class Ahorcado {
 
     private int checkIfWordIsTheWordHidden(String hangedWord, String hiddenWord){
         String inputWord;
-        int cont = 0;
 
+        System.out.println("Introduce una palabra: ");
         inputWord = sc.nextLine();
+
         if(hangedWord.equals(inputWord)){
             System.out.println("Has acertado, la palabra era: " + hangedWord + ", has necesitado " + cont + " intentos");
         }else{
@@ -73,145 +66,33 @@ public class Ahorcado {
         }
 
         //se tiene que comprobar si ha acertado todas las letras
+        int contGuiones = 0;
         for (int i = 0; i < hiddenWord.length(); i++) {
             if(hiddenWord.charAt(i)=='_'){
-
+                contGuiones++;
             }
         }
 
+        if(contGuiones==0)
+            System.out.println("Has acertado, la palabra era: " + hangedWord + ", has necesitado " + cont + " intentos");
+        else
+            System.out.println(cont + " intentos, te quedan " + (15-cont));//15=intentosMax
+
         System.out.println(hiddenWord);
-        System.out.println("contador: " + cont);
 
         return cont;
     }
 
-    private void drawing(int cont){
-        String hangingDraw;
 
-        switch (cont){
-            case 1:
-                hangingDraw = "______";
-                break;
-            case 2:
-                hangingDraw = "__|__";
-                break;
-            case 3:
-                hangingDraw =
-                        "  | \n" +
-                        "__|__";
-            case 4:
-                hangingDraw =
-                        "  | \n" +
-                        "  | \n" +
-                                "__|__";
-                break;
-            case 5:
-                hangingDraw =
-                        "  | \n" +
-                        "  | \n" +
-                                "  | \n" +
-                                "__|__";
-                break;
-            case 6:
-                hangingDraw =
-                        "  | \n" +
-                        "  | \n" +
-                                "  | \n" +
-                                "  | \n" +
-                                "__|__";
-                break;
-            case 7:
-                hangingDraw =
-                        "------" +
-                        "  | \n" +
-                                "  | \n" +
-                                "  | \n" +
-                                "  | \n" +
-                                "__|__";
-                break;
-            case 8:
-                hangingDraw =
-                        "------" +
-                                "  | \n" +
-                                "  | \n" +
-                                "  | \n" +
-                                "  | \n" +
-                                "__|__";
-                break;
-            case 9:
-                hangingDraw =
-                        "------" +
-                                "  |   |\n" +
-                                "  | \n" +
-                                "  | \n" +
-                                "  | \n" +
-                                "__|__";
-                break;
-            case 10:
-                hangingDraw =
-                        "------" +
-                                "  |   |\n" +
-                                "  |   O\n" +
-                                "  | \n" +
-                                "  | \n" +
-                                "__|__";
-                break;
-            case 11:
-                hangingDraw =
-                        "------" +
-                                "  |   |\n" +
-                                "  |   O\n" +
-                                "  |   |\n" +
-                                "  | \n" +
-                                "__|__";
-                break;
-            case 12:
-                hangingDraw =
-                        "------" +
-                                "  |   |\n" +
-                                "  |   O\n" +
-                                "  |   |\n" +
-                                "  |   /\n" +
-                                "__|__";
-                break;
-            case 13:
-                hangingDraw =
-                        "------" +
-                                "  |   |\n" +
-                                "  |   O\n" +
-                                "  |   |\n" +
-                                "  |   /\\ " + "\n "+
-                                "__|__";
-                break;
-            case 14:
-                hangingDraw =
-                        "------" +
-                                "  |   |\n" +
-                                "  |   O\n" +
-                                "  |  /|\n" +
-                                "  |   /\\ " + "\n "+
-                                "__|__";
-                break;
-            case 15:
-                hangingDraw =
-                        "------" +
-                                "  |   |\n" +
-                                "  |   O\n" +
-                                "  |  /|\\n" +
-                                "  |   /\\ " + "\n "+
-                                "__|__";
-                System.out.println("Has muerto, vuelve a intentarlo en otro momento");
-                break;
-
-
-        }
+    public void finalOutput(){
+        DrawingHanged d = new DrawingHanged();
+        d.drawing(checkIfWordIsTheWordHidden(getHangedWord(chooseNumberXSelectWord()),
+                getHiddenWord(getPalabraAdivinar(chooseNumberXSelectWord()))));
     }
 
 
-
-
-
     public static void main(String[] args) {
-
+        Ahorcado ahorcado = new Ahorcado();
+        ahorcado.finalOutput();
     }
 }
---
+++ DrawingHanged.java
@@ -0,0 +1,125 @@
+package desafios;
+
+public class DrawingHanged {
+    protected void drawing(int cont){
+        String hangingDraw;
+
+        switch (cont){
+            case 1:
+                hangingDraw = "______";
+                break;
+            case 2:
+                hangingDraw = "__|__";
+                break;
+            case 3:
+                hangingDraw =
+                        "  | \n" +
+                                "__|__";
+            case 4:
+                hangingDraw =
+                        "  | \n" +
+                                "  | \n" +
+                                "__|__";
+                break;
+            case 5:
+                hangingDraw =
+                        "  | \n" +
+                                "  | \n" +
+                                "  | \n" +
+                                "__|__";
+                break;
+            case 6:
+                hangingDraw =
+                        "  | \n" +
+                                "  | \n" +
+                                "  | \n" +
+                                "  | \n" +
+                                "__|__";
+                break;
+            case 7:
+                hangingDraw =
+                        "------" +
+                                "  | \n" +
+                                "  | \n" +
+                                "  | \n" +
+                                "  | \n" +
+                                "__|__";
+                break;
+            case 8:
+                hangingDraw =
+                        "------" +
+                                "  | \n" +
+                                "  | \n" +
+                                "  | \n" +
+                                "  | \n" +
+                                "__|__";
+                break;
+            case 9:
+                hangingDraw =
+                        "------" +
+                                "  |   |\n" +
+                                "  | \n" +
+                                "  | \n" +
+                                "  | \n" +
+                                "__|__";
+                break;
+            case 10:
+                hangingDraw =
+                        "------" +
+                                "  |   |\n" +
+                                "  |   O\n" +
+                                "  | \n" +
+                                "  | \n" +
+                                "__|__";
+                break;
+            case 11:
+                hangingDraw =
+                        "------" +
+                                "  |   |\n" +
+                                "  |   O\n" +
+                                "  |   |\n" +
+                                "  | \n" +
+                                "__|__";
+                break;
+            case 12:
+                hangingDraw =
+                        "------" +
+                                "  |   |\n" +
+                                "  |   O\n" +
+                                "  |   |\n" +
+                                "  |   /\n" +
+                                "__|__";
+                break;
+            case 13:
+                hangingDraw =
+                        "------" +
+                                "  |   |\n" +
+                                "  |   O\n" +
+                                "  |   |\n" +
+                                "  |   /\\ " + "\n "+
+                                "__|__";
+                break;
+            case 14:
+                hangingDraw =
+                        "------" +
+                                "  |   |\n" +
+                                "  |   O\n" +
+                                "  |  /|\n" +
+                                "  |   /\\ " + "\n "+
+                                "__|__";
+                break;
+            case 15:
+                hangingDraw =
+                        "------" +
+                                "  |   |\n" +
+                                "  |   O\n" +
+                                "  |  /|\\n" +
+                                "  |   /\\ " + "\n "+
+                                "__|__";
+                System.out.println("Has muerto, vuelve a intentarlo en otro momento");
+                break;
+
+
+        }
+    }
+}
